12f93584a9a9080151d417c4856bab3e7f2c2224
= <inman.turbo@gmail.com>
2024-01-31 15:34:50 -0500

    format code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Inmanturbo\ContactsManager\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Inmanturbo\ContactsManager\ContactsManagerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
